e0827bd5572a601fe685214516dd3030333501ca
ltmervin123 <ltmervin123@gmail.com>
2024-11-11 06:29:49 +0800

    Authentication and authorization

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './page/LandingPage.jsx';
-import Login from './page/LoginPage.jsx';
-import SignUp from './page/SignUpPage.jsx';
-import MaindashboardPage from './page/MaindashboardPage.jsx';
-import Analytics from './page/Analytics.jsx';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import LandingPage from "./page/LandingPage.jsx";
+import Login from "./page/LoginPage.jsx";
+import SignUp from "./page/SignUpPage.jsx";
+import MaindashboardPage from "./page/MaindashboardPage.jsx";
+import Analytics from "./page/Analytics.jsx";
+import { useAuthContext } from "./hook/useAuthContext";
 
 function App() {
+  const { user } = useAuthContext();
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/maindashboard" />}
+        />
         <Route path="/" element={<LandingPage />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/analytics" element={<Analytics />} />
-        <Route path="/maindashboard" element={<MaindashboardPage />} />
+        <Route
+          path="/signup"
+          element={!user ? <SignUp /> : <Navigate to="/maindashboard" />}
+        />
+        <Route
+          path="/analytics"
+          element={user ? <Analytics /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/maindashboard"
+          element={user ? <MaindashboardPage /> : <Navigate to="/login" />}
+        />
       </Routes>
     </Router>
   );
