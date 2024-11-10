753b46b1be55d1f168bfdb9de98fcae4502a811d
ltmervin123 <ltmervin123@gmail.com>
2024-11-10 19:22:30 +0800

    Add log in but noy finish yet

--- src/hook/useLogin.js
+++ src/hook/useLogin.js
@@ -2,19 +2,19 @@ import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
 import axios from "axios";
 
-export const useSignup = () => {
+export const useLogin = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
-  const URL = "http://localhost:5000/api/user/auth/signup";
+  const URL = "http://localhost:5000/api/user/auth/login";
   const { dispatch } = useAuthContext();
 
-  const signup = async (name, email, password) => {
+  const login = async (email, password) => {
     setIsLoading(true);
     setError(null);
 
     try {
       //Request body
-      const requestBody = { name, email, password };
+      const requestBody = { email, password };
 
       // Make POST request with axios
       const response = await axios.post(URL, requestBody, {
@@ -24,23 +24,21 @@ export const useSignup = () => {
       const user = await response.data;
 
       // Check if the response is successful
-      if (response.status === 201) {
+      if (response.status === 200) {
         //Set the user to the local storage
         localStorage.setItem("user", JSON.stringify(user));
 
         // Dispatch the user data to context or handle login state
         dispatch({ type: "LOGIN", payload: user });
       }
-      return true;
     } catch (err) {
       // Handle any error from the request
       setError(err.response ? err.response.data.error : "Signup failed");
-      return false;
     } finally {
       // Set loading to false after the request completes
       setIsLoading(false);
     }
   };
 
-  return { signup, isLoading, error };
+  return { login, isLoading, error };
 };
