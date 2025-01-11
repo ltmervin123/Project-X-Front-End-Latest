edd912faa96f0db374d1053bc019e1d2e06f6b45
ltmervin123 <ltmervin123@gmail.com>
2025-01-11 20:02:13 +0800

    implement interviewer option on basic interview

--- src/components/maindashboard/InterviewerOption.jsx
+++ src/components/maindashboard/InterviewerOption.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Modal, Row, Col, Button } from 'react-bootstrap';
-import interviewerImg from '../../assets/mock-avatar.png'; // Make sure to add this image to your assets
+import React from "react";
+import { Modal, Row, Col, Button } from "react-bootstrap";
+import interviewerImg from "../../assets/mock-avatar.png"; // Make sure to add this image to your assets
 
 const InterviewerOption = ({ show, onHide, onSelectInterviewer }) => {
   return (
@@ -11,43 +11,44 @@ const InterviewerOption = ({ show, onHide, onSelectInterviewer }) => {
       className="interviewer-option-modal"
     >
       <Modal.Body className="p-4">
-                <div className="d-flex justify-content-between align-items-center mb-3">
-                <h4>Choose Your Interviewer</h4>
-                <Button
-                  className='closebtn'
-                    variant="link"
-                    onClick={onHide}
-                    style={{ fontSize: "1.5rem", textDecoration: "none" }}
-                  >
-                    &times;
-                  </Button>
-                </div>
-        
+        <div className="d-flex justify-content-between align-items-center mb-3">
+          <h4>Choose Your Interviewer</h4>
+          <Button
+            className="closebtn"
+            variant="link"
+            onClick={onHide}
+            style={{ fontSize: "1.5rem", textDecoration: "none" }}
+          >
+            &times;
+          </Button>
+        </div>
+
         <Row>
           <Col md={6} className="d-flex flex-column gap-3">
-            <Button 
+            <Button
               className="interviewer-btn"
               onClick={() => {
-                console.log('Selected interviewer: Steve');
-                onSelectInterviewer('Steve');
+                onSelectInterviewer("Steve");
               }}
             >
               Steve
             </Button>
-            <Button 
+            <Button
               className="interviewer-btn"
               onClick={() => {
-                console.log('Selected interviewer: Stela');
-                onSelectInterviewer('Stela');
+                onSelectInterviewer("Stela");
               }}
             >
               Stela
             </Button>
           </Col>
-          <Col md={6} className="d-flex align-items-center justify-content-center">
-            <img 
-              src={interviewerImg} 
-              alt="Interviewer" 
+          <Col
+            md={6}
+            className="d-flex align-items-center justify-content-center"
+          >
+            <img
+              src={interviewerImg}
+              alt="Interviewer"
               className="interviewer-image"
             />
           </Col>
