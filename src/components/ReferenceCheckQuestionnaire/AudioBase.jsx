8bcac7a897c3092ee1ccc825eb5d39597d58c00a
ltmervin123 <ltmervin123@gmail.com>
2025-03-08 00:13:05 +0800

    Update socket io connection

--- src/components/ReferenceCheckQuestionnaire/AudioBase.jsx
+++ src/components/ReferenceCheckQuestionnaire/AudioBase.jsx
@@ -1,10 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { FaMicrophone, FaMicrophoneAltSlash } from "react-icons/fa";
-import {
-  socket,
-  connectSocket,
-  disconnectSocket,
-} from "../../utils/socket/socketSetup";
+import { socket } from "../../utils/socket/socketSetup";
 import axios from "axios";
 
 const AudioBase = ({
@@ -42,13 +38,9 @@ const AudioBase = ({
   }, [streamRef]);
 
   useEffect(() => {
-    if (!socket.connected) {
-      connectSocket();
-    }
     socket.emit("startTranscription");
 
     return () => {
-      disconnectSocket();
       socket.off("real-time-transcription");
       socket.off("final-transcription");
       socket.off("transcription-complete");
