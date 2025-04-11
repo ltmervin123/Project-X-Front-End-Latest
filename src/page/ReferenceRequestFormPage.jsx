8faaa20f17210522bee97c0e7dfbc0694ef4f022
ltmervin123 <ltmervin123@gmail.com>
2025-04-12 05:45:55 +0800

    new implementation reference flow initial commit

--- src/page/ReferenceRequestFormPage.jsx
+++ src/page/ReferenceRequestFormPage.jsx
@@ -5,7 +5,6 @@ function ReferenceRequestFormPage() {
   return (
     <>
       <div className="py-5 bg-gray d-flex justify-content-center align-items-center h-100">
-      {/* <div className="container-fluid main-container"> */}
         <ReferenceRequestForm />
       </div>
     </>
