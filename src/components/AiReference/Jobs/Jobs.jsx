2bccb8b211c5186a3159aba75dc786b0343a05dc
ltmervin123 <ltmervin123@gmail.com>
2025-05-09 23:06:46 +0800

    Add onRefetchJobs prop to AddVacancyComponent for job refetching

--- src/components/AiReference/Jobs/Jobs.jsx
+++ src/components/AiReference/Jobs/Jobs.jsx
@@ -179,6 +179,7 @@ const Jobs = () => {
           onBack={() => setShowAddVacancy(false)}
           jobData={job}
           onCancel={() => setShowAddVacancy(false)}
+          onRefetchJobs={refetchJobs}
         />
       ) : (
         <>
