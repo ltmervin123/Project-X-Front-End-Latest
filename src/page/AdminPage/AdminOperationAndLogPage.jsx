7721b16ec03d20db865f10832610249ab03baab4
Aivankun <110333665+Aivankun@users.noreply.github.com>
2025-05-10 21:46:33 +0800

    pull req

--- src/page/AdminPage/AdminOperationAndLogPage.jsx
+++ src/page/AdminPage/AdminOperationAndLogPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Row, Col } from "react-bootstrap";
 import Header from "../../components/Admin/Header";
 import Sidebar from "../../components/Admin/Sidebar";
+import TranslationDropdown from "../../components/Admin/TranslationDropdown";
 import "../../styles/AdminStyles/AnalyticsDashboardPage.css";
 
 function AdminOperationAndLogPage() {
@@ -14,7 +15,9 @@ function AdminOperationAndLogPage() {
             <Sidebar />
           </Col>
 
-          <Col md={10} className="p-3"></Col>
+          <Col md={10} className="p-3">
+          <TranslationDropdown />
+          </Col>
         </Row>
       </div>
     </>
