4902e8ab87e5f25ac439fb1ad921925a25fb4a0d
ltmervin123 <ltmervin123@gmail.com>
2025-05-25 18:02:33 +0800

    Add @tanstack/eslint-plugin-query to devDependencies and update ESLint configuration for improved query management

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
-  extends: ['react-app'],
+  extends: ["react-app", "plugin:@tanstack/query/recommended"],
+
   rules: {
-    'no-unused-vars': 'warn',
-    'react-hooks/exhaustive-deps': 'warn',
-    'jsx-a11y/alt-text': 'warn',
-    'jsx-a11y/anchor-is-valid': 'warn',
-    'jsx-a11y/heading-has-content': 'warn'
-  }
-}; 
+    "no-unused-vars": "warn",
+    "react-hooks/exhaustive-deps": "warn",
+    "jsx-a11y/alt-text": "warn",
+    "jsx-a11y/anchor-is-valid": "warn",
+    "jsx-a11y/heading-has-content": "warn",
+  },
+};
