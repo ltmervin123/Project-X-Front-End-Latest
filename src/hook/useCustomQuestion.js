b1e6abc8e3ee75005e1089d8880092358651a0c6
ltmervin123 <ltmervin123@gmail.com>
2025-05-27 21:37:38 +0800

    Refactor useCustomQuestion hook by  increasing query caching from 1 mins to 10 mins and add createCustomQuestion mutation

--- src/hook/useCustomQuestion.js
+++ src/hook/useCustomQuestion.js
@@ -3,14 +3,15 @@ import {
   getCustomReferenceQuestions,
   deleteCustomReferenceQuestion,
   updateCustomReferenceQuestion,
+  createCustomReferenceQuestion,
 } from "../api/ai-reference/custom-questions/custom-questions-api";
 
 export const useGetCustomQuestions = (user) => {
   return useQuery({
     queryKey: ["custom-questions", user],
     queryFn: () => getCustomReferenceQuestions(user),
-    staleTime: 1000 * 60 * 1,
-    refetchInterval: 1000 * 60 * 1,
+    staleTime: 1000 * 60 * 10,
+    refetchInterval: 1000 * 60 * 10,
     refetchIntervalInBackground: true,
   });
 };
@@ -42,6 +43,23 @@ export const useUpdateCustomQuestion = (user, options = {}) => {
   return useMutation({
     mutationFn: async ({ questionId, payload }) => {
       await updateCustomReferenceQuestion({ user, questionId, payload });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["custom-questions"],
+      });
+    },
+    onSettled: onSettled,
+  });
+};
+
+export const useCreateCustomQuestion = (user, options = {}) => {
+  const queryClient = useQueryClient();
+  const { onSettled } = options;
+
+  return useMutation({
+    mutationFn: async (payload) => {
+      await createCustomReferenceQuestion({ payload, user });
     },
     onSuccess: () => {
       queryClient.invalidateQueries({
