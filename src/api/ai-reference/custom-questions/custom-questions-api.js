7e14527f6a6b5269b91658a9ee2f1b30c52664b6
ltmervin123 <ltmervin123@gmail.com>
2025-05-27 20:44:45 +0800

    Refactor custom questions API by adding function for updating and deleting of custom question

--- src/api/ai-reference/custom-questions/custom-questions-api.js
+++ src/api/ai-reference/custom-questions/custom-questions-api.js
@@ -11,3 +11,28 @@ export const getCustomReferenceQuestions = async (user) => {
   });
   return response.data.questions;
 };
+
+export const deleteCustomReferenceQuestion = async (user, questionId) => {
+  const { id, token } = user;
+  const URL = `${API}/api/ai-referee/company-reference-questions/delete-reference-questions/${questionId}/${id}`;
+  await axios.delete(URL, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const updateCustomReferenceQuestion = async (param) => {
+  const { user, questionId, payload } = param;
+  const { token } = user;
+  const URL = `${API}/api/ai-referee/company-reference-questions/update-reference-questions/${questionId}`;
+  await axios.put(
+    URL,
+    { payload },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+};
