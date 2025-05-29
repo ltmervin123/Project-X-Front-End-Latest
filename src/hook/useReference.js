0db9670449cf2423b186c186097f224daa0fba76
ltmervin123 <ltmervin123@gmail.com>
2025-05-29 13:50:06 +0800

    Add deleteReference and useGetReferencesById hooks for reference management

--- src/hook/useReference.js
+++ src/hook/useReference.js
@@ -1,7 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 import {
   getCompletedReferences,
   getReferences,
+  deleteReference,
+  fetchReferenceByReferenceId,
 } from "../api/ai-reference/reference-request/reference-request-api";
 
 export const useGetCompletedReference = (user) => {
@@ -21,5 +23,30 @@ export const useGetReferences = (user) => {
     staleTime: 1000 * 60 * 1,
     refetchInterval: 1000 * 60 * 1,
     refetchIntervalInBackground: true,
+  });
+};
+
+export const useDeleteReference = (user, options = {}) => {
+  const queryClient = useQueryClient();
+  const { onSettled } = options;
+
+  return useMutation({
+    mutationFn: async (referenceId) => {
+      await deleteReference({ user, referenceId });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["references"] });
+      queryClient.invalidateQueries({ queryKey: ["archivedReferenceRequest"] });
+    },
+    onSettled: onSettled,
+  });
+};
+
+export const useGetReferencesById = (params) => {
+  return useQuery({
+    queryKey: ["reference", params],
+    queryFn: () => fetchReferenceByReferenceId(params),
+    staleTime: 1000 * 60 * 5,
+    enabled: !!params.referenceId && !!params.refereeId && !!params.token,
   });
 };
