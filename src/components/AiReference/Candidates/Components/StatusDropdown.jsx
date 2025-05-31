ca0963c06c4aa5368036171d744c48cde278373a
ltmervin123 <ltmervin123@gmail.com>
2025-05-31 15:58:55 +0800

    Refactor status handling in Applicant and CandidatesTable components; integrate status update functionality in StatusDropdown

--- src/components/AiReference/Candidates/Components/StatusDropdown.jsx
+++ src/components/AiReference/Candidates/Components/StatusDropdown.jsx
@@ -1,33 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useUpdateCandidateStatus } from "../../../../hook/useCandidate";
 
-const StatusDropdown = ({ status, onChange, getStatusColor, translations }) => {
+const StatusDropdown = ({
+  status,
+  getStatusColor,
+  translations,
+  user,
+  candidate,
+}) => {
+  const { mutate: updateCandidateStatus, isPending: isUpdating } =
+    useUpdateCandidateStatus(user, {
+      onSettled: () => {},
+    });
   const [isOpen, setIsOpen] = useState(false);
 
-  if (status === 'Accept' || status === 'Reject') {
+  if (status === "Accept" || status === "Reject") {
     return (
-      <div 
-        className="status-display"
-        style={{ color: getStatusColor(status) }}
-      >
+      <div className="status-display" style={{ color: getStatusColor(status) }}>
         {translations[`Status_${status}`]}
       </div>
     );
   }
 
   const options = [
-    { value: 'Accept', label: translations.Status_Accept },
-    { value: 'Reject', label: translations.Status_Reject }
+    { value: "Accepted", label: translations.Status_Accept },
+    { value: "Rejected", label: translations.Status_Reject },
   ];
 
-  const handleSelect = (value) => {
-    onChange(value);
+  const handleSelect = (status) => {
+    const candidateId = candidate._id;
+    updateCandidateStatus({ candidateId, status });
     setIsOpen(false);
   };
 
+  if (isUpdating) {
+    return (
+      <div className="d-flex justify-content-center align-items-center">
+        <div
+          className="spinner-border spinner-border-sm text-primary"
+          role="status"
+        ></div>
+      </div>
+    );
+  }
+
   return (
-    <div className={`custom-status-dropdown ${isOpen ? 'open' : ''}`}>
-      <div 
-        className={`status-trigger ${isOpen ? 'open' : ''}`}
+    <div className={`custom-status-dropdown ${isOpen ? "open" : ""}`}>
+      <div
+        className={`status-trigger ${isOpen ? "open" : ""}`}
         onClick={() => setIsOpen(!isOpen)}
         style={{ color: getStatusColor(status) }}
       >
@@ -40,7 +60,7 @@ const StatusDropdown = ({ status, onChange, getStatusColor, translations }) => {
               key={option.value}
               className="status-option"
               onClick={() => handleSelect(option.value)}
-              style={{ color: getStatusColor(option.value) }}
+              style={{ color: getStatusColor(option.label) }}
             >
               {option.label}
             </div>
