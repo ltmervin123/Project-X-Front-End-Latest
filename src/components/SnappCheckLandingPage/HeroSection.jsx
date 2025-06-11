3c9d198025337a6b484a03c539cbb09215667d33
Aivankun <110333665+Aivankun@users.noreply.github.com>
2025-06-12 02:00:35 +0800

    Optimize image rendering in landing page fix

--- src/components/SnappCheckLandingPage/HeroSection.jsx
+++ src/components/SnappCheckLandingPage/HeroSection.jsx
@@ -5,7 +5,7 @@ import { useSnappcheckTranslation } from "./hooks/snappcheckTranslation";
 import HeroImg from "../../assets/snappchecklanding/hero-img.svg";
 
 // HeroSection functional component
-const HeroSection = () => {
+const HeroSection = ({isHeroVisible}) => {
   const { t } = useSnappcheckTranslation();
 
   return (
@@ -13,27 +13,25 @@ const HeroSection = () => {
       {/* Hero Section Container */}
       <section
         id="hero"
-        className={
-          "snappcheck-hero-container d-flex align-items-center  w-100 h-100"
-        }
+        className="snappcheck-hero-container d-flex align-items-center w-100 h-100"
       >
-        <Row className="w-100 h-100 d-flex justify-content-center ">
-               
+        <Row className="w-100 h-100 d-flex justify-content-center">
+          <div className={`snappcheck-hero-content d-flex justify-content-center flex-column position-relative gap-4 fade-in ${
+            isHeroVisible ? "visible" : ""
+          }`}>
+            <img src={HeroImg} alt="" />
 
-          <div className="snappcheck-hero-content d-flex justify-content-center flex-column position-relative gap-4">
-          <img src={HeroImg} alt="" />
-
-            <h1 className="mb-5 mt-3 ">
+            <h1 className="mb-5 mt-3">
               {t("heroTitle")
                 .split("SNAPPCHECK")
                 .map((part, index, array) => {
                   if (index === array.length - 1) return part;
                   return (
-                    <>
+                    <React.Fragment key={index}>
                       {part}
                       <span className="color-orange">SNAPP</span>
                       <span className="color-grey">CHECK</span>
-                    </>
+                    </React.Fragment>
                   );
                 })}
             </h1>
