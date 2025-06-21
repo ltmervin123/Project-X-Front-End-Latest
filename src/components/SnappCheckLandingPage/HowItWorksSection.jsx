bfea12e3dc59d7db3beb1081fe159fe75f99a6f4
Aivankun <110333665+Aivankun@users.noreply.github.com>
2025-06-21 18:57:03 +0800

    New Snappcheck Landing Page 100%

--- src/components/SnappCheckLandingPage/HowItWorksSection.jsx
+++ src/components/SnappCheckLandingPage/HowItWorksSection.jsx
@@ -1,13 +1,6 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import { useSnappcheckTranslation } from './hooks/snappcheckTranslation';
-// import HowItWorkImage from "../../assets/snappchecklanding/how-it-works.svg";
-import HowItWorkImage1 from "../../assets/snappchecklanding/howitwork1.svg";
-import HowItWorkImage2 from "../../assets/snappchecklanding/howitwork2.svg";
-import HowItWorkImage3 from "../../assets/snappchecklanding/howitwork3.svg";
-import HowItWorkImage4 from "../../assets/snappchecklanding/howitwork4.svg";
-import HowItWorkImage5 from "../../assets/snappchecklanding/howitwork5.svg";
-import HowItWorkImage6 from "../../assets/snappchecklanding/howitwork6.svg";
 
 // HowItWorksSection functional component
 const HowItWorksSection = ({ isHowItWorksVisible }) => {
@@ -18,52 +11,26 @@ const HowItWorksSection = ({ isHowItWorksVisible }) => {
       {/* How It Works Section Container */}
       <section
         id="how-it-works"
-        className={`snappcheck-did-you-know-container d-flex align-items-center flex-column w-100 fade-in ${
-          isHowItWorksVisible ? "visible" : ""
-        }`}
+        className={`snappcheck-how-it-works-container d-flex align-items-center flex-column w-100 fade-in ${isHowItWorksVisible ? "visible" : ""
+          }`}
       >
-        <Row className="w-100 snappcheck-left-did-you-know-content">
-          <Col md={6}>
-          <div className="d-flex justify-content-center align-items-start flex-column">
-            <h3 className="color-blue text-center mb-2">{t('howItWorks')}</h3>
-            <h2 className="how-it-works-title" style={{ letterSpacing: '2px' }}>
-              {t('referenceCheckFlow').includes('Flow') ? (
-                t('referenceCheckFlow').split('Flow').map((part, index, array) => (
-                  <React.Fragment key={index}>
-                    {part}
-                    {index < array.length - 1 && <span className="color-orange">Flow</span>}
-                  </React.Fragment>
-                ))
-              ) : (
-                t('referenceCheckFlow')
-              )}
+        <Row className=" position-relative">
+
+          <div className="snappcheck-how-it-works-content d-flex justify-content-center align-items-center flex-column">
+            <h1>{t('howItWorksSectionTitle')}</h1>
+            <h2 className="mb-5 "> <p className="how-it-works-subtitle m-0">
+              <span className="color-orange">{t('howItWorksSectionSubtitle')}</span>
+            </p>
             </h2>
 
-          </div></Col>
-          <Col md={6}>
-               {/* Cards Section */}
-               <div className="d-flex justify-content-center align-items-center flex-column mb-4 w-100">
-            <div className="how-it-works-cards-container">
-              {[
-                { img: HowItWorkImage1, title: t('howItWorksStep1') },
-                { img: HowItWorkImage2, title: t('howItWorksStep2') },
-                { img: HowItWorkImage3, title: t('howItWorksStep3') },
-                { img: HowItWorkImage4, title: t('howItWorksStep4') },
-                { img: HowItWorkImage5, title: t('howItWorksStep5') },
-                { img: HowItWorkImage6, title: t('howItWorksStep6') },
-              ].map((card, idx) => (
-                <div key={idx} className="how-it-works-card d-flex flex-column align-items-center p-3 m-2">
-                  <div className="how-it-works-card-number mb-2">{idx + 1}</div>
-                  <img src={card.img} alt={`how it works step ${idx + 1}`} />
-                  <div className="how-it-works-card-title text-center">{card.title}</div>
-                </div>
-              ))}
+            <div className="how-it-works-video-container">
+            <video controls>
+  <source src="movie.mp4" type="video/mp4" />
+  <source src="movie.ogg" type="video/ogg" />
+  {t('videoNotSupported')}
+</video>
             </div>
-          </div></Col>
-
-       
-
-     
+          </div>
         </Row>
       </section>
     </>
--- src/components/SnappCheckLandingPage/PricingSection.jsx
+++ src/components/SnappCheckLandingPage/PricingSection.jsx
@@ -1,7 +1,7 @@
 import React, { forwardRef } from "react";
 import { Row, Col } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { useSnappcheckTranslation } from './hooks/snappcheckTranslation';
+import { useSnappcheckTranslation } from "./hooks/snappcheckTranslation";
 
 // PricingSection functional component
 const PricingSection = forwardRef(({ isPricingVisible }, ref) => {
@@ -10,25 +10,25 @@ const PricingSection = forwardRef(({ isPricingVisible }, ref) => {
 
   const plans = [
     {
-      title: t('starterTitle'),
-      description: t('starterDesc'),
+      title: t('starterPlan'),
+      description: t('starterPlanDesc'),
       price: "27,500 JPY",
       subscriptionRange: t('monthlyIncludingTax'),
-      subtext: t('starterSubtext'),
+      subtext: t('includes5Cases'),
       isPopular: false,
     },
     {
-      title: t('proTitle'),
-      description: t('proDesc'),
+      title: t('professionalPlan'),
+      description: t('professionalPlanDesc'),
       price: "44,000 JPY",
       subscriptionRange: t('monthlyIncludingTax'),
-      subtext: t('proSubtext'),
+      subtext: t('includes10Cases'),
       isPopular: true,
     },
     {
-      title: t('enterpriseTitle'),
-      description: t('customSolutions'),
-      price: t('priceCustom'),
+      title: t('enterprisePlan'),
+      description: t('enterprisePlanDesc'),
+      price: t('custom'),
       subscriptionRange: t('contactForQuotation'),
       subtext: t('unlimitedReferenceChecks'),
       isEnterprise: true,
@@ -37,12 +37,12 @@ const PricingSection = forwardRef(({ isPricingVisible }, ref) => {
 
   const handleButtonClick = (isEnterprise) => {
     if (isEnterprise) {
-      const contactSection = document.getElementById('contact-us');
+      const contactSection = document.getElementById("contact-us");
       if (contactSection) {
-        contactSection.scrollIntoView({ behavior: 'smooth' });
+        contactSection.scrollIntoView({ behavior: "smooth" });
       }
     } else {
-      navigate('/PaymentMethod');
+      navigate("/PaymentMethod");
     }
   };
 
@@ -50,61 +50,52 @@ const PricingSection = forwardRef(({ isPricingVisible }, ref) => {
     <section
       ref={ref}
       id="pricing"
-      className={`snappcheck-pricing-container d-flex align-items-center w-100 fade-in ${
+      className={`snappcheck-pricing-container d-flex align-items-center flex-column w-100 fade-in ${
         isPricingVisible ? "visible" : ""
       }`}
     >
-      <Row className="w-100 snappcheck-left-content">
-        <h3 className="color-grey text-center mb-2">{t('pricingTitle')}</h3>
-        <h2 className="mb-4 text-center w-100">
-          {language === 'English' ? (
-            t('pricingSubtitle').split('Right Plan').map((part, index, array) => (
-              <React.Fragment key={index}>
-                {part}
-                {index < array.length - 1 && <span className="color-orange">Right Plan</span>}
-              </React.Fragment>
-            ))
-          ) : (
-            t('pricingSubtitle').split('最適なプラン').map((part, index, array) => (
-              <React.Fragment key={index}>
-                {part}
-                {index < array.length - 1 && <span className="color-orange">最適なプラン</span>}
-              </React.Fragment>
-            ))
-          )}
-        </h2>
-        
-        <div className="pricing-cards-container">
-          {plans.map((plan, index) => (
-            <div
-              key={index}
-              className={`pricing-card d-flex flex-column align-items-center justify-content-between ${
-                plan.isPopular ? "popular" : ""
-              } ${plan.isEnterprise ? "enterprise" : ""}`}
-            >
-              <div className="d-flex flex-column justify-content-between mt-3">
-                {plan.isPopular && (
-                  <span className="popular-badge">{t('mostPopular')}</span>
-                )}
-                <h3>{plan.title}</h3>
-                <p className="m-0">{plan.description}</p>
-                <div className="price">{plan.price || "\u00A0"}</div>
-                <p className="subscription-range m-0">{plan.subscriptionRange}</p>
+      <Row className="position-relative">
+        <div className="snappcheck-pricing-content d-flex justify-content-center align-items-center flex-column">
+          <h1>{t('pricingSectionTitle')}</h1>
+          <h2 className="pricing-subtitle">
+            {t('pricingSectionSubtitle')} <span className="color-orange">{t('rightPlan')}</span>{" "}
+          </h2>
 
-                {plan.subtext && (
-                  <p className="pricing-subtext m-0 d-flex gap-2">
-                    <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path fillRule="evenodd" clipRule="evenodd" d="M8 17C9.05058 17 10.0909 16.7801 11.0615 16.353C12.0321 15.9258 12.914 15.2997 13.6569 14.5104C14.3997 13.7211 14.989 12.7841 15.391 11.7528C15.7931 10.7215 16 9.61624 16 8.5C16 7.38376 15.7931 6.27846 15.391 5.24719C14.989 4.21592 14.3997 3.27889 13.6569 2.48959C12.914 1.70029 12.0321 1.07419 11.0615 0.647024C10.0909 0.219859 9.05058 -1.66332e-08 8 0C5.87827 3.35923e-08 3.84344 0.895533 2.34315 2.48959C0.842855 4.08365 0 6.24566 0 8.5C0 10.7543 0.842855 12.9163 2.34315 14.5104C3.84344 16.1045 5.87827 17 8 17ZM7.79378 11.9378L12.2382 6.27111L10.8729 5.06222L7.05067 9.93461L5.07289 7.83228L3.816 9.16772L6.48267 12.0011L7.17067 12.7321L7.79378 11.9378Z" fill="#F46A05"/>
-                    </svg>
-                    {plan.subtext}
+          <div className="pricing-cards-container mt-5">
+            {plans.map((plan, index) => (
+              <div
+                key={index}
+                className={`pricing-card d-flex flex-column align-items-center justify-content-between ${
+                  plan.isPopular ? "popular" : ""
+                } ${plan.isEnterprise ? "enterprise" : ""}`}
+              >
+                <div className="d-flex flex-column justify-content-between mt-3">
+                  {plan.isPopular && (
+                    <span className="popular-badge">{t('mostPopular')}</span>
+                  )}
+                  <p className="price-title">{plan.title}</p>
+                  <p className="price-desc">{plan.description}</p>
+                  <div className="price">{plan.price || "\u00A0"}</div>
+                  <p className="subscription-range">
+                    {plan.subscriptionRange}
                   </p>
-                )}
+
+                  {plan.subtext && (
+                    <p className="pricing-subtext d-flex gap-2">
+                      
+                      {plan.subtext}
+                    </p>
+                  )}
+                </div>
+                <button
+                  className="choose-plan-btn"
+                  onClick={() => handleButtonClick(plan.isEnterprise)}
+                >
+                  {plan.isEnterprise ? t('contactForQuote') : t('choosePlan')}
+                </button>
               </div>
-              <button className="choose-plan-btn" onClick={() => handleButtonClick(plan.isEnterprise)}>
-                {plan.isEnterprise ? t('contactForQuotes') : t('chooseplan')}
-              </button>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </Row>
     </section>
